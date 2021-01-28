02ebfaec40c58fbe12ed13939c0ff03889f5ce17
nyborgt <tim.nyborg@conted.ox.ac.uk>
2021-01-28 11:41:56 +0000

    layout cleanup, try out regex validation

--- apps/module/models.py
+++ apps/module/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.core.validators import RegexValidator
 from django.db.models import Model, CharField, DateField, ForeignKey, IntegerField, BooleanField, ImageField, DO_NOTHING, Q
 from django.core.exceptions import ValidationError
 from django.template.defaultfilters import slugify
 from django.urls import reverse
 
+from apps.main.models import SignatureModel
 
-class Module(Model):
+
+class Module(SignatureModel, Model):
     code = CharField(max_length=12, help_text='For details on codes, see <link>')    
     title = CharField(max_length=80)
     url = models.SlugField(max_length=256, blank=True, null=True)
@@ -24,6 +27,93 @@ class Module(Model):
     max_size = IntegerField(blank=True, null=True)
 
     image = ImageField(upload_to='uploads/%Y/%m/%d/', max_length=512, blank=True, null=True)
+
+    # type = models.ForeignKey('ModuleType', models.DO_NOTHING, db_column='type', blank=True, null=True)
+
+    start_time = models.TimeField(blank=True, null=True)
+    end_time = models.TimeField(blank=True, null=True)
+    publish_date = models.DateField(blank=True, null=True)
+    open_date = models.DateField(blank=True, null=True)
+    closed_date = models.DateTimeField(blank=True, null=True)
+    unpublish_date = models.DateField(blank=True, null=True)
+
+    single_places = models.IntegerField(blank=True, null=True)
+    twin_places = models.IntegerField(blank=True, null=True)
+    double_places = models.IntegerField(blank=True, null=True)
+    # location = models.ForeignKey(Location, models.DO_NOTHING, db_column='location', blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    meeting_time = models.CharField(max_length=32, blank=True, null=True)
+    duration = models.FloatField(blank=True, null=True)
+    no_meetings = models.IntegerField(blank=True, null=True)
+
+    auto_publish = models.BooleanField()
+
+    is_published = models.BooleanField()
+    # finance_code = models.CharField(max_length=64, blank=True, null=True)
+    email = models.CharField(max_length=256, blank=True, null=True)
+    phone = models.CharField(max_length=256, blank=True, null=True)
+
+    source_module_code = models.CharField(max_length=12, blank=True, null=True)
+    overview = models.TextField(blank=True, null=True)
+    accommodation = models.TextField(blank=True, null=True)
+    application = models.TextField(blank=True, null=True)
+    assessment_methods = models.TextField(blank=True, null=True)
+    certification = models.TextField(blank=True, null=True)
+    course_aims = models.TextField(blank=True, null=True)
+    level_and_demands = models.TextField(blank=True, null=True)
+    libraries = models.TextField(blank=True, null=True)
+    payment = models.TextField(blank=True, null=True)
+    programme_details = models.TextField(blank=True, null=True)
+    recommended_reading = models.TextField(blank=True, null=True)
+    scholarships = models.TextField(blank=True, null=True)
+    snippet = models.CharField(max_length=512, blank=True, null=True)
+    teaching_methods = models.TextField(blank=True, null=True)
+    teaching_outcomes = models.TextField(blank=True, null=True)
+    selection_criteria = models.TextField(blank=True, null=True)
+    it_requirements = models.TextField(blank=True, null=True)
+    full_time_equivalent = models.DecimalField(max_digits=4, decimal_places=1)
+    credit_points = models.IntegerField(blank=True, null=True)
+    points_level = models.IntegerField(blank=True, null=True)
+    enrol_online = models.BooleanField(blank=True, null=True)
+    non_credit_bearing = models.BooleanField()
+    auto_feedback = models.BooleanField()
+    auto_reminder = models.BooleanField()
+    no_search = models.BooleanField()
+    week_number = models.IntegerField(blank=True, null=True)
+
+    custom_fee = models.CharField(max_length=1012, blank=True, null=True)
+    # format = models.ForeignKey('ModuleFormat', models.DO_NOTHING, db_column='format', blank=True, null=True)
+
+    is_cancelled = models.BooleanField()
+    default_non_credit = models.BooleanField(blank=True, null=True)
+    note = models.CharField(max_length=512, blank=True, null=True)
+    # terms_and_conditions = models.ForeignKey('TermsAndConditions', models.DO_NOTHING, db_column='terms_and_conditions')
+    apply_url = models.CharField(max_length=512, blank=True, null=True)
+    further_details = models.TextField(blank=True, null=True)
+    is_repeat = models.BooleanField()
+    reminder_sent_on = models.DateTimeField(blank=True, null=True)
+    room = models.CharField(max_length=12, blank=True, null=True)
+    room_setup = models.CharField(max_length=12, blank=True, null=True)
+
+    mailing_list = models.CharField(max_length=25, blank=True, null=True)
+    notification = models.CharField(max_length=512, blank=True, null=True)
+    cost_centre = models.CharField(max_length=6, blank=True, null=True)
+    activity_code = models.CharField(
+        max_length=2, blank=True, null=True,
+        validators=[RegexValidator(r'^\d{2}$', message='Invalid code')],
+        help_text='e.g. 00',
+    )
+    source_of_funds = models.CharField(
+        max_length=5, blank=True, null=True,
+        validators=[RegexValidator(r'^\w{5}$', message='Invalid code')],
+        help_text='e.g. XA100',
+    )
+    fee_code = models.CharField(max_length=1, blank=True, null=True)
+
+    half_term = models.DateField(blank=True, null=True)
+
+    reading_list_url = models.TextField(blank=True, null=True)
+    reading_list_links = models.BooleanField(blank=True, null=True)
 
     class Meta:
         managed = False
