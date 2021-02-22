cad347b50daef7f35c5c1b2b71da666f18206f8a
nyborgt <tim.nyborg@conted.ox.ac.uk>
2021-02-22 12:24:58 +0000

    refine enrolment model

--- apps/enrolment/models.py
+++ apps/enrolment/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 from apps.core.models import SignatureModel
 
+# Constants used as defaults.  If used more extensively, we may need to use enums
+NOT_CODED_RESULT = 7
+
 
 class Enrolment(SignatureModel):
     qa = models.ForeignKey('programme.QA', models.DO_NOTHING, db_column='qa', blank=True, null=True)
     module = models.ForeignKey('module.Module', models.DO_NOTHING, db_column='module', related_name='enrolments')
     status = models.ForeignKey('EnrolmentStatus', models.DO_NOTHING, db_column='status')
-    result = models.ForeignKey('EnrolmentResult', models.DO_NOTHING, db_column='result', default=7)
+    result = models.ForeignKey(
+        'EnrolmentResult', models.DO_NOTHING, db_column='result',
+        limit_choices_to={'is_active': True},
+        default=NOT_CODED_RESULT
+    )
     points_awarded = models.IntegerField(blank=True, null=True)
-    provenance = models.IntegerField(blank=True, null=True)
-    provenance_details = models.CharField(max_length=128, blank=True, null=True)
-    no_image_consent = models.BooleanField(blank=True, null=True)
     mark = models.IntegerField(blank=True, null=True)
-    transcript_date = models.DateTimeField(blank=True, null=True)
+    transcript_date = models.DateTimeField(blank=True, null=True, editable=False)
 
     class Meta:
         # managed = False
@@ -26,11 +30,16 @@ class Enrolment(SignatureModel):
 
 
 class EnrolmentResult(SignatureModel):
+    """
+        `id` is a terrifying bit of backwards compatibility.
+        The PK used to be HESA's 1-9 + A-C, but then values like '2.1' were added for special cases
+        It should all be replaced with integers, but that will require a survey of its use in reporting
+    """
     id = models.CharField(primary_key=True, max_length=4)
     description = models.CharField(max_length=128, blank=True, null=True)
-    is_active = models.BooleanField()
+    is_active = models.BooleanField(default=True)
     display_order = models.IntegerField(blank=True, null=True)
-    hesa_code = models.CharField(max_length=1, blank=True, null=True)
+    hesa_code = models.CharField(max_length=1)
     allow_certificate = models.BooleanField(blank=True, null=True)
 
     class Meta:
