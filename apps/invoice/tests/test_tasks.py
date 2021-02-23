8a6c8fb240be1cdfdbf5b45ea01f639b46eec6db
nyborgt <tim.nyborg@conted.ox.ac.uk>
2021-02-23 18:05:41 +0000

    basic rcp tests

---
+++ apps/invoice/tests/test_tasks.py
@@ -0,0 +1,34 @@
+from unittest.mock import patch, Mock, ANY
+from ftplib import FTP_TLS
+
+from django.test import TestCase
+
+from ..tasks import wpm_ftp_download
+
+
+class TestWPMDownload(TestCase):
+
+    @patch('ftplib.FTP_TLS', autospec=True)
+    def test_requests_correct_file(self, mock_class):
+        filename = 'test.csv'
+
+        # Patch the returned object's nlst() method so the file appears available
+        mock_ftp_obj = mock_class()
+        mock_ftp_obj.nlst.return_value = [filename]
+
+        wpm_ftp_download(filename)
+        # Check that RETR was called
+        mock_ftp_obj.retrlines.assert_called_with(f"RETR {filename}", ANY)
+
+    @patch('ftplib.FTP_TLS', autospec=True)
+    def test_missing_file_error(self, mock_class):
+        filename = 'missing.csv'
+
+        # Patch the returned object's nlst() method so the file appears unavailable
+        mock_ftp_obj = mock_class()
+        mock_ftp_obj.nlst.return_value = ['other.csv']
+
+        with self.assertRaises(FileNotFoundError):
+            wpm_ftp_download(filename)
+
+    # todo: test parsing and db methods
