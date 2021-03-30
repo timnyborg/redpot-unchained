fe6fd0c4a8e9daf9adca6ef1bf0389fa3825d906
Sivakumar, Lokesh <lokesh.sivakumar@conted.ox.ac.uk>
2021-03-30 16:11:21 +0000

    web publication moved to seperate section of its own

--- apps/module/urls.py
+++ apps/module/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('search', views.Search.as_view(), name='search'),
     path('edit/<int:pk>', views.Edit.as_view(), name='edit'),
     path('view/<int:pk>', views.View.as_view(), name='view'),
+    path('toggle-auto-reminder/<int:pk>', views.toggle_auto_reminder, name='toggle-auto-reminder'),
+    path('toggle-auto-feedback/<int:pk>', views.toggle_auto_feedback, name='toggle-auto-feedback'),
 ]
