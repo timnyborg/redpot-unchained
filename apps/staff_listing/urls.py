c120ae28cca27da8845d47bd62d6d8c3d6e4d199
Lokesh Sivakumar <lokesh.sivakumar@conted.ox.ac.uk>
2021-07-07 12:56:36 +0100

    removed unwanted codes, added SiteTitleMixin

--- apps/staff_listing/urls.py
+++ apps/staff_listing/urls.py
@@ -3,10 +3,9 @@ from . import views
 
 app_name = 'staff_listing'
 urlpatterns = [
-    path('', views.index),
+    path('', views.WallListView.as_view(), name='home'),
     path('wall', views.WallListView.as_view(), name='wall'),
     path('list', views.StaffListView.as_view(), name='list'),
     path('profile/<int:pk>', views.StaffDetailView.as_view(), name='profile'),
-    path('courses', views.CoursesListView.as_view(), name='courses'),
-    #path('courses', views.courses, name='courses'),
+    path('courses', views.CoursesListView.as_view(), name='courses')
 ]
