832c9601ba07dc62e744145251ed83fd4e99f1fe
nyborgt <tim.nyborg@conted.ox.ac.uk>
2021-07-29 08:44:45 +0100

    module api sets timestamps

--- apps/module/api.py
+++ apps/module/api.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from rest_framework import generics, permissions
 
 from . import models, serializers
@@ -7,3 +9,9 @@ class ModuleUpdateAPI(generics.UpdateAPIView):
     queryset = models.Module.objects.all()
     serializer_class = serializers.ModuleSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def perform_update(self, serializer):
+        serializer.save(
+            modified_by=self.request.user.username,
+            modified_on=datetime.now(),
+        )
