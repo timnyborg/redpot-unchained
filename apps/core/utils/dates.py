018f50c31337b37b3d42f39414157b4ae3216012
nyborgt <tim.nyborg@conted.ox.ac.uk>
2021-08-09 16:35:53 +0100

    typing for academic_year

--- apps/core/utils/dates.py
+++ apps/core/utils/dates.py
@@ -1,12 +1,13 @@
-from datetime import date
+import datetime
+from typing import Optional
 
 
-def academic_year(_date: date = None):
+def academic_year(date: Optional[datetime.date] = None) -> int:
     """Get the academic year of a given date or datetime
     Academic years run 1 Aug - 31
 
-    If not provided with a _date, today is used
+    If not provided with a `date`, today is used
     """
-    if not _date:
-        _date = date.today()
-    return _date.year - 1 if _date.month < 8 else _date.year
+    if not date:
+        date = datetime.date.today()
+    return date.year - int(date.month < 8)
