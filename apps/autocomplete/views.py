62277817b431726f6601fbdf638456b0bec6cd8a
nyborgt <tim.nyborg@conted.ox.ac.uk>
2021-08-24 15:46:47 +0100

    enrolment autocomplete endpoint

--- apps/autocomplete/views.py
+++ apps/autocomplete/views.py
@@ -2,6 +2,7 @@ from dal import autocomplete
 
 from django.db.models import Q
 
+from apps.enrolment.models import Enrolment
 from apps.module.models import Module
 from apps.tutor.models import Tutor
 
@@ -9,11 +10,9 @@ from apps.tutor.models import Tutor
 class ModuleAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
         qs = Module.objects.all()
-
         if self.q:
             # Filter on name or code containing the string
             qs = qs.filter(Q(title__unaccent__icontains=self.q) | Q(code__icontains=self.q)).order_by('-id')
-
         return qs
 
     def get_result_label(self, result):
@@ -23,11 +22,23 @@ class ModuleAutocomplete(autocomplete.Select2QuerySetView):
 class TutorAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
         qs = Tutor.objects.select_related('student')
-
         if self.q:
             # Filter on name or code containing the string
             qs = qs.filter(
                 Q(student__firstname__unaccent__icontains=self.q) | Q(student__surname__icontains=self.q)
             ).order_by('student__firstname', 'student__surname')
-
         return qs
+
+
+class EnrolmentAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        qs = Enrolment.objects.select_related('qa__student', 'module')
+        if self.q:
+            # Filter on name or code containing the string
+            qs = qs.filter(
+                Q(module__title__unaccent__icontains=self.q) | Q(module__code__icontains=self.q),
+            ).order_by('-id', 'qa__student__firstname', 'qa__student__surname')
+        return qs
+
+    def get_result_label(self, result):
+        return f'{result.module.code} - {result.qa.student}'
