6f1bf88b3ca5526c1e4a3703518227c6457f5e4a
nyborgt <tim.nyborg@conted.ox.ac.uk>
2021-08-31 16:54:33 +0100

    no-ie middleware

--- redpot/settings.py
+++ redpot/settings.py
@@ -143,6 +143,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'redpot.middleware.IEDetectionMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
