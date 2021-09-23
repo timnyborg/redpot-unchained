72e4c7698708b5f761c073a63847e10ecc4d37a8
Bee, Asia <beea@mail.conted.ox.ac.uk>
2021-09-23 11:16:24 +0000

    Student details forms

--- apps/student/tests/test_views.py
+++ apps/student/tests/test_views.py
@@ -96,8 +96,8 @@ class TestCreateEmail(TestCase):
     def setUpTestData(cls):
         cls.user = get_user_model().objects.create_user(username='testuser')
         cls.student = factories.StudentFactory()
-        cls.url = reverse('student:email-create', kwargs={'student_id': cls.student.pk})
-        cls.invalid_url = reverse('student:email-create', kwargs={'student_id': 0})
+        cls.url = reverse('student:email:new', kwargs={'student_id': cls.student.pk})
+        cls.invalid_url = reverse('student:email:new', kwargs={'student_id': 0})
 
     def setUp(self):
         self.client.force_login(self.user)
@@ -121,6 +121,30 @@ class TestCreateEmail(TestCase):
     def test_invalid_student_returns_404(self):
         response = self.client.get(self.invalid_url)
         self.assertEqual(response.status_code, 404)
+
+
+class TestEmailsViews(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(username='testuser')
+        cls.student = factories.StudentFactory()
+        cls.email = factories.EmailFactory(student=cls.student)
+
+    def setUp(self):
+        self.client.force_login(self.user)
+
+    def test_edit_email(self):
+        self.client.post(reverse('student:email:edit', kwargs={'pk': self.email.pk}), {'email': 'email1@test.com'})
+        self.email.refresh_from_db()
+        self.assertEqual(self.email.email, 'email1@test.com')
+
+    def test_delete_email(self):
+        new_email = factories.EmailFactory(student=self.student)
+        self.assertEqual(self.email.student, new_email.student)
+        self.assertEquals(self.student.emails.count(), 2)
+        self.client.post(reverse('student:email:delete', kwargs={'pk': self.email.pk}))
+        self.student.refresh_from_db()
+        self.assertEqual(self.student.emails.count(), 1)
 
 
 class TestMakeTutor(TestCase):
@@ -262,6 +286,129 @@ class TestCreateAddress(LoggedInViewTestMixin, TestCase):
         self.assertEqual(response.status_code, 302)
         address = models.Address.objects.last()
         self.assertEqual(address.student_id, self.student.id)
+
+
+class TestMoodleIdViews(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(username='testuser')
+        cls.MOODLE_ID = 8902567
+        cls.student = factories.StudentFactory()
+        cls.url = reverse('student:moodle-id:new', kwargs={'student_id': cls.student.id})
+
+    def setUp(self):
+        self.client.force_login(self.user)
+
+    def test_moodle_id(self):
+        response = self.client.post(
+            self.url, data={'student': self.student.pk, 'moodle_id': self.MOODLE_ID, 'first_module_code': 123}
+        )
+        self.assertEqual(response.status_code, 302)
+        moodle_id = models.MoodleID.objects.last()
+        self.assertEqual(moodle_id.student_id, self.student.id)
+
+    def test_edit_moodle_id(self):
+        self.moodle = factories.MoodleFactory(student=self.student)
+        self.client.post(reverse('student:moodle-id:edit', kwargs={'pk': self.moodle.id}), {'moodle_id': 876987987})
+        self.moodle.refresh_from_db()
+        self.assertEqual(self.moodle.moodle_id, 876987987)
+
+    def test_delete_moodle_id(self):
+        self.moodle = factories.MoodleFactory(student=self.student, moodle_id=self.MOODLE_ID)
+        self.assertEqual(self.moodle.moodle_id, self.MOODLE_ID)
+        self.client.post(reverse('student:moodle-id:delete', kwargs={'pk': self.moodle.pk}))
+        with self.assertRaises(models.MoodleID.DoesNotExist):
+            self.moodle.refresh_from_db()
+
+
+class TestPhone(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(username='testuser')
+        cls.student = factories.StudentFactory()
+        cls.url = reverse('student:phone:new', kwargs={'student_id': cls.student.pk})
+        cls.invalid_url = reverse('student:phone:new', kwargs={'student_id': 0})
+
+    def setUp(self):
+        self.client.force_login(self.user)
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_creates_phone(self):
+        response = self.client.post(
+            self.url,
+            data={
+                'student': self.student.pk,
+                'number': '+(44) 1865 245252',
+                'type': models.Phone.PhoneTypeChoices.PHONE,
+                'is_default': True,
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(self.student.phones.first().number, '+(44) 1865 245252')
+        self.assertTrue(self.student.phones.first().is_default)
+
+    def test_edit_phone(self):
+        self.phone = factories.PhoneFactory(student=self.student)
+        response = self.client.post(
+            reverse('student:phone:edit', kwargs={'pk': self.phone.pk}),
+            data={'number': '+(92) 1675 2445252', 'type': models.Phone.PhoneTypeChoices.FAX},
+        )
+        self.phone.refresh_from_db()
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(self.phone.number, '+(92) 1675 2445252')
+
+    def test_invalid_student_phone_returns_404(self):
+        response = self.client.get(self.invalid_url)
+        self.assertEqual(response.status_code, 404)
+
+
+class TestOtherID(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(username='testuser')
+        cls.student = factories.StudentFactory()
+        cls.url = reverse('student:other-id:new', kwargs={'student_id': cls.student.pk})
+        cls.invalid_url = reverse('student:other-id:new', kwargs={'student_id': 0})
+
+    def setUp(self):
+        self.client.force_login(self.user)
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_creates_other_id(self):
+        response = self.client.post(
+            self.url,
+            data={
+                'student': self.student.pk,
+                'number': '2345678998765',
+                'type': models.OtherID.OtherIdTypeChoices.STUDENT_SUPPORT_NUM,
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(self.student.other_ids.first().number, '2345678998765')
+        self.assertEqual(
+            self.student.other_ids.first().type, models.OtherID.OtherIdTypeChoices.STUDENT_SUPPORT_NUM.value
+        )
+
+    def test_edit_other_id(self):
+        self.other_id = factories.OtherIDFactory(student=self.student)
+        response = self.client.post(
+            reverse('student:other-id:edit', kwargs={'pk': self.other_id.pk}),
+            {'number': '2345678998765', 'type': models.OtherID.OtherIdTypeChoices.VISA_ID},
+        )
+        self.other_id.refresh_from_db()
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(self.other_id.number, '2345678998765')
+        self.assertEqual(self.student.other_ids.first().type, models.OtherID.OtherIdTypeChoices.VISA_ID)
+
+    def test_invalid_student_other_id_returns_404(self):
+        response = self.client.get(self.invalid_url)
+        self.assertEqual(response.status_code, 404)
 
 
 class TestEditStudent(LoggedInViewTestMixin, TestCase):
--- apps/student/urls.py
+++ apps/student/urls.py
@@ -15,6 +15,42 @@ address_urls = (
     'address',
 )
 
+email_urls = (
+    [
+        path('new/<int:student_id>', views.CreateEmail.as_view(), name='new'),
+        path('edit/<int:pk>', views.EditEmail.as_view(), name='edit'),
+        path('delete/<int:pk>', views.DeleteEmail.as_view(), name='delete'),
+    ],
+    'email',
+)
+
+phone_urls = (
+    [
+        path('new/<int:student_id>', views.CreatePhone.as_view(), name='new'),
+        path('edit/<int:pk>', views.EditPhone.as_view(), name='edit'),
+        path('delete/<int:pk>', views.DeletePhone.as_view(), name='delete'),
+    ],
+    'phone',
+)
+
+other_id_urls = (
+    [
+        path('new/<int:student_id>', views.CreateOtherID.as_view(), name='new'),
+        path('edit/<int:pk>', views.EditOtherID.as_view(), name='edit'),
+        path('delete/<int:pk>', views.DeleteOtherID.as_view(), name='delete'),
+    ],
+    'other-id',
+)
+
+moodle_id_urls = (
+    [
+        path('new/<int:student_id>', views.CreateMoodleID.as_view(), name='new'),
+        path('edit/<int:pk>', views.EditMoodleID.as_view(), name='edit'),
+        path('delete/<int:pk>', views.DeleteMoodleID.as_view(), name='delete'),
+    ],
+    'moodle-id',
+)
+
 urlpatterns = [
     path('make-tutor/<int:student_id>', views.MakeTutor.as_view(), name='make-tutor'),
     path('new', views.Create.as_view(), name='new'),
@@ -22,8 +58,11 @@ urlpatterns = [
     path('lookup', views.Lookup.as_view(), name='lookup'),
     path('edit/<int:pk>', views.Edit.as_view(), name='edit'),
     path('view/<int:pk>', views.View.as_view(), name='view'),
+    path('email/', include(email_urls)),
+    path('phone/', include(phone_urls)),
+    path('other-id/', include(other_id_urls)),
+    path('moodle-id/', include(moodle_id_urls)),
     path('delete/<int:pk>', views.Delete.as_view(), name='delete'),
-    path('email/create/<int:student_id>', views.CreateEmail.as_view(), name='email-create'),
     path('address/', include(address_urls)),
     path('merge/', not_implemented, name='merge'),
     # apis
