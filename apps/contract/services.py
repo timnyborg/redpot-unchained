22bdbd2d443d8f0ef6cee1cb011821dd5d7956f1
nyborgt <tim.nyborg@conted.ox.ac.uk>
2021-09-29 11:58:54 +0100

    contract email notification service

--- apps/contract/services.py
+++ apps/contract/services.py
@@ -1,5 +1,10 @@
 from datetime import datetime
 
+from django.conf import settings
+from django.core import mail
+from django.template.loader import render_to_string
+from django.utils import html
+
 from apps.contract import models
 from apps.core.utils.postal import FormattedAddress
 from apps.tutor.models import RightToWorkType
@@ -22,3 +27,24 @@ def generate_fixed_properties(*, contract: models.Contract) -> dict:
         'list_a_rtw': tutor.rtw_type == RightToWorkType.PERMANENT,
         'overseas_rtw': tutor.rtw_type == RightToWorkType.OVERSEAS,
     }
+
+
+def send_notification_mail(*, contract: models.Contract) -> int:
+    """Email the contract's notification address (when it is signed).  Returns True if an email was sent"""
+    if not contract.email_notification:
+        return 0
+    context = {
+        'tutor_name': contract.options['full_name'],
+        'module_name': contract.tutor_module.module.title,
+        'module_code': contract.tutor_module.module.code,
+        'url': settings.CANONICAL_URL + contract.get_absolute_url(),
+    }
+    message = render_to_string('contract/email/signed_notification.html', context=context)
+    recipients = [settings.SUPPORT_EMAIL if settings.DEBUG else contract.email_notification]
+    return mail.send_mail(
+        subject='Contract - signed',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=recipients,
+        message=html.strip_tags(message),
+        html_message=message,
+    )
