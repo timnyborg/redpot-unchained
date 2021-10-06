db45f35ddbed667f0fbfda07b9ee89a724e9d2e7
Tim Nyborg <tim.nyborg@conted.ox.ac.uk>
2021-10-06 11:29:52 +0100

    remove bs4

--- apps/core/static/js/redpot.js
+++ apps/core/static/js/redpot.js
@@ -26,31 +26,3 @@ jQuery(function() {
     //Default collapsed
     hide_row.trigger('click', 0);
 });
-
-//Autogenerate sidenavs.  Any nav-anchor <a> will be added, with the following H3's title, overridable by giving the nav-anchor a data-title attr.
-// Legacy.  Replaced with function in tools for BS5
-jQuery(function() {
-    const linklist = $('#sidebar ul:first');
-    if (linklist.length) {
-        $('a.nav-anchor').each(function() {
-            let title;
-            if ($(this).data('title')) {
-                title = $(this).data('title');
-            } else {
-                title = $(this).next('h2').text();
-            }
-            let badge = '';
-            if ($(this).data('badge-text')) {
-                if ($(this).data('badge-class')) {
-                    badge = ' <span class="badge badge-' + $(this).data('badge-class') + '">' + $(this).data('badge-text') + '</span>';
-                }
-                else {
-                    badge = ' <span class="badge">' + $(this).data('badge-text') + '</span>';
-                }
-            }
-            linklist.append('<li><a href="#' + this.id + '"><span class="fa fa-chevron-left"></span>' + title + badge + '</a></li>');
-        });
-
-        $('body').scrollspy();
-    }
-});
