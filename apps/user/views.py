96f2b4a365e9cacc5a13e096b80a62999b72f9f4
Tim Nyborg <tim.nyborg@conted.ox.ac.uk>
2021-10-06 11:29:09 +0100

    remaining apps on bs5

--- apps/user/views.py
+++ apps/user/views.py
@@ -10,12 +10,15 @@ class EditProfile(PageTitleMixin, SuccessMessageMixin, LoginRequiredMixin, Updat
     """View allowing a member of staff to edit their own profile / preferences"""
 
     model = User
-    fields = ('first_name', 'last_name', 'role', 'phone', 'room', 'image')
+    fields = ('first_name', 'last_name', 'email', 'role', 'phone', 'room', 'image')
     template_name = 'core/form.html'
     subtitle = 'Edit profile'
     subtitle_object = False
     success_message = 'Profile updated'
     success_url = '/'
 
-    def get_object(self, queryset=None):
+    def get_object(self, queryset=None) -> User:
         return self.request.user
+
+    def get_success_url(self) -> str:
+        return self.object.get_absolute_url()
