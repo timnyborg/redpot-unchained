f15924f2cb4eeaf64e7e5c0bbe1c150695d098a9
Tim Nyborg <tim.nyborg@conted.ox.ac.uk>
2021-10-18 13:49:31 +0100

    default_approver field

--- apps/user/views.py
+++ apps/user/views.py
@@ -5,20 +5,18 @@ from django.views.generic.edit import UpdateView
 from apps.core.models import User
 from apps.core.utils.views import PageTitleMixin
 
+from . import forms
+
 
 class EditProfile(PageTitleMixin, SuccessMessageMixin, LoginRequiredMixin, UpdateView):
-    """View allowing a member of staff to edit their own profile / preferences"""
+    """Allows a member of staff to edit their own profile / preferences"""
 
     model = User
-    fields = ('first_name', 'last_name', 'email', 'role', 'phone', 'room', 'image')
+    form_class = forms.UserForm
     template_name = 'core/form.html'
     subtitle = 'Edit profile'
     subtitle_object = False
     success_message = 'Profile updated'
-    success_url = '/'
 
     def get_object(self, queryset=None) -> User:
         return self.request.user
-
-    def get_success_url(self) -> str:
-        return self.object.get_absolute_url()
