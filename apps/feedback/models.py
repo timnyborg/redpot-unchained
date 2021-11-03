e4cad54b95079624fda9a9d2786e5e2b3b8e9cc9
Sivakumar, Lokesh <lokesh.sivakumar@conted.ox.ac.uk>
2021-11-03 11:51:07 +0000

    Feedback updated

--- apps/feedback/models.py
+++ apps/feedback/models.py
@@ -1,17 +1,29 @@
+import datetime
 import uuid
 
 from django.db import models
 
 
+class FeedbackQueryset(models.QuerySet):
+    def get_year_range_queryset(self, diff):
+        current_year = datetime.datetime.now().year
+        self.from_year = current_year - diff
+        self.to_year = current_year
+        return self.filter(
+            module__start_date__gt=datetime.date(self.from_year, 8, 31),
+            module__start_date__lt=datetime.date(self.to_year, 9, 1),
+        )
+
+
 class Feedback(models.Model):
-    module = models.ForeignKey('module.Module', models.DO_NOTHING, db_column='module', null=True)
+    module = models.ForeignKey('module.Module', models.DO_NOTHING, db_column='module', null=False)
     rate_tutor = models.IntegerField(blank=True, null=True)
     rate_content = models.IntegerField(blank=True, null=True)
     rate_admin = models.IntegerField(blank=True, null=True)
     rate_facilities = models.IntegerField(blank=True, null=True)
     rate_refreshments = models.IntegerField(blank=True, null=True)
     rate_accommodation = models.IntegerField(blank=True, null=True)
-    your_name = models.TextField(blank=True, null=True)
+    your_name = models.CharField(blank=True, null=True, max_length=128)
     hash_id = models.CharField(default=uuid.uuid4, editable=False, unique=True, max_length=40)
     enrolment = models.IntegerField(blank=True, null=True)
     notified = models.DateTimeField(blank=True, null=True)
@@ -23,9 +35,11 @@ class Feedback(models.Model):
     class Meta:
         db_table = 'feedback'
 
+    objects = FeedbackQueryset.as_manager()
+
 
 class FeedbackAdmin(models.Model):
-    module = models.ForeignKey('module.Module', models.DO_NOTHING, db_column='module', null=True)
+    module = models.ForeignKey('module.Module', models.DO_NOTHING, db_column='module', null=False)
     updated = models.DateTimeField(blank=True, null=True)
     admin_comments = models.TextField(blank=True, null=True)
     person = models.TextField(blank=True, null=True)
--- apps/feedback/templatetags/feedback_tags.py
+++ apps/feedback/templatetags/feedback_tags.py
@@ -6,7 +6,7 @@ register = template.Library()
 @register.simple_tag
 def score_color(score):
     """Color formats the score value"""
-    if isinstance(score, int) or isinstance(score, float):
+    if isinstance(score, (int, float)):
         if score > 3.5:
             return 'feedback-green'
         elif score > 2.5:
