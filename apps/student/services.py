e3558b7d96e2a4a0723421c97203f4880cbeff4a
Tim Nyborg <tim.nyborg@conted.ox.ac.uk>
2021-11-09 00:06:02 +0000

    functional merge

--- apps/student/services.py
+++ apps/student/services.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
 from datetime import datetime
-from typing import Iterator
+from typing import Any, Iterator
 
+from django.db import transaction
 from django.db.models import Max
+
+from apps.tutor.models import Tutor
 
 from . import models
 
@@ -99,3 +102,174 @@ def supplement_student_data(*, student: models.Student, **kwargs) -> None:
         setattr(student, field, value)
     student.full_clean()
     student.save()
+
+
+class CannotMergeError(Exception):
+    """Indicates that business logic disallows merging two student records"""
+
+
+def _order_by_husid(student: models.Student) -> tuple:
+    """Order husids by priority: Oldest (99 before 00), and student (integer) before enquiry (null)"""
+    year = (student.husid or 0) // 10 ** 11  # e.g. 95, 00, 21
+    return student.husid is None, year < 90, year
+
+
+def merge_multiple_students(students: list[models.Student]) -> dict:
+    """Merge multiple students, their ids passed in a list"""
+    target, *sources = sorted(students, key=_order_by_husid)
+
+    results = {'target': target.id, 'merges': []}
+
+    for source in sources:
+        merge_result = {
+            # todo: why?
+            'id': source.id,
+            'name': str(source),
+            'husid': source.husid,
+        }
+
+        try:
+            merge_students(source=source, target=target)
+            merge_result['error'] = False
+        except CannotMergeError as error:
+            merge_result['error'] = error
+
+        results['merges'].append(merge_result)
+
+    return results
+
+
+@transaction.atomic
+def merge_students(*, source: models.Student, target: models.Student):
+    # todo: docstring
+    # todo: refactor into testable sub-functions
+    if hasattr(source, 'tutor') and hasattr(target, 'tutor'):
+        raise CannotMergeError("Both records are tutors.  This can't be handled yet.")
+    if source.sits_id and target.sits_id and source.sits_id != target.sits_id:
+        raise CannotMergeError('Conflicting SITS IDs')
+
+    create_merge_archive_record(source=source, target=target)
+
+    def get_default(field: str) -> Any:
+        return models.Student._meta.get_field(field).default
+
+    # Overwrite a number of target's student items if it is None or in the unknowns, while source isn't
+    field_unknowns = {
+        'birthdate': [],
+        'ethnicity_id': [get_default('ethnicity')],
+        'domicile_id': [get_default('domicile')],
+        'nationality_id': [get_default('nationality')],
+        'religion_or_belief_id': [get_default('religion_or_belief')],
+        'gender': [],
+        'sits_id': [],
+        # Preserve any opt-ins
+        'email_optin': [False],
+        'email_optin_on': [],
+        'email_optin_method': [''],
+        'mail_optin': [False],
+        'mail_optin_on': [],
+        'mail_optin_method': [''],
+    }
+
+    for field, unknowns in field_unknowns.items():
+        if getattr(target, field) in [None] + unknowns and getattr(source, field) not in [None] + unknowns:
+            setattr(target, field, getattr(source, field))
+
+    # Mergeable data - reassign source data that doesn't yet exist on the target to the target
+    # todo: consider replicating redpot-legacy's existence checking, to avoid duplicates
+    source.emails.update(student=target)
+    source.phones.update(student=target)
+    source.addresses.update(student=target)
+    source.other_ids.update(student=target)
+    source.website_accounts.update(student=target)
+
+    # Set contact defaults to last modified if any exist
+    # for table in (
+    #     'address',
+    #     'email',
+    #     'phone',
+    # ):
+    #     newest_data = idb((idb[table].student == target.id)).select(orderby=~idb[table].modified_on).first()
+    #     if newest_data:
+    #         newest_data.update_record(is_default=True)
+
+    # For logins, make most recently modified in combined set active
+    target.website_accounts.update(is_disabled=True)
+    newest_login = target.website_accounts.order_by('-modified_on').first()
+    if newest_login:
+        newest_login.is_disabled = False
+        newest_login.save()
+
+    # Child tables that follow a normal many-to-one structure (data never overlaps)
+    # todo: move up?
+    source.enquiries.update(student=target)
+    source.suspensions.update(student=target)
+    source.waitlists.update(student=target)
+
+    Tutor.objects.filter(student=source).update(student=target)
+    merge_one_to_ones(source=source, target=target)
+    merge_qualification_aims(source=source, target=target)
+
+    target.save()
+    source.delete()
+
+
+def merge_one_to_ones(*, source: models.Student, target: models.Student) -> None:
+    for model in ('moodle_id', 'diet', 'emergency_contact'):
+        source_child = getattr(source, model, None)
+        if source_child:
+            if hasattr(target, model):
+                source_child.delete()
+            else:
+                source_child.student = target
+                source_child.save()
+
+
+def merge_qualification_aims(*, source: models.Student, target: models.Student) -> None:
+    for qa in source.qualification_aims.all():
+        # Check for a matching qualification aim on the target, and check that the source isn't award bearing
+        matching_qa = target.qualification_aims.filter(programme_id=qa.programme_id).first()
+        if matching_qa and not qa.programme.qualification.is_award:
+            # Move enrolments
+            qa.enrolments.update(qa=matching_qa.id)
+            qa.delete()
+        else:
+            # Move entire QA
+            qa.student = target
+            qa.save()
+
+
+def create_merge_archive_record(*, source: models.Student, target: models.Student) -> None:
+    ...
+    # Create a JSON archive of the student and child data
+    # archive = (
+    #     idb(idb.student.id == source.id)
+    #     .select(
+    #         idb.student.ALL,
+    #         idb.address.ALL,
+    #         idb.email.ALL,
+    #         idb.phone.ALL,
+    #         idb.qa.ALL,
+    #         idb.enrolment.ALL,
+    #         idb.login.ALL,
+    #         idb.other_id.ALL,
+    #         idb.diet.ALL,
+    #         idb.moodle_id.ALL,
+    #         left=[
+    #             idb.address.on(idb.address.student == idb.student.id),
+    #             idb.email.on(idb.email.student == idb.student.id),
+    #             idb.phone.on(idb.phone.student == idb.student.id),
+    #             idb.qa.on(idb.qa.student == idb.student.id),
+    #             idb.enrolment.on(idb.enrolment.qa == idb.qa.id),
+    #             idb.login.on(idb.login.student == idb.student.id),
+    #             idb.other_id.on(idb.other_id.student == idb.student.id),
+    #             idb.diet.on(idb.diet.student == idb.student.id),
+    #             idb.moodle_id.on(idb.moodle_id.student == idb.student.id),
+    #         ],
+    #     )
+    #     .as_json()
+    # )
+
+    # todo: create json via model_to_dict or django.core.serializers.serialize, attaching children manually
+
+    # idb.student_archive.insert(source=source.id, target=target.id, husid=source.husid, json=archive)
--- apps/student/tests/test_services.py
+++ apps/student/tests/test_services.py
@@ -1,8 +1,13 @@
+from datetime import date
+
 from parameterized import parameterized
 
 from django.test import SimpleTestCase, TestCase
 
+from apps.tutor.tests.factories import TutorFactory
+
 from .. import models, services
+from . import factories
 
 
 class TestGenerateHUSID(SimpleTestCase):
@@ -30,3 +35,38 @@ class TestGettingNextHUSID(TestCase):
     def test_validation(self):
         self.assertRaises(ValueError, services.next_husid, 0)
         self.assertRaises(ValueError, services.next_husid, 9999)
+
+
+class TestMerge(TestCase):
+    def test_merging_tutors_raises_error(self):
+        source, target = TutorFactory.create_batch(size=2)
+        with self.assertRaisesRegex(services.CannotMergeError, 'tutor'):
+            services.merge_students(source=source.student, target=target.student)
+
+    def test_merging_sits_students_raises_error(self):
+        source = factories.StudentFactory(sits_id=1)
+        target = factories.StudentFactory(sits_id=2)
+        with self.assertRaisesRegex(services.CannotMergeError, 'SITS'):
+            services.merge_students(source=source, target=target)
+
+    def test_merging_overrides_defaults(self):
+        source = factories.StudentFactory(birthdate=date(2000, 1, 1), nationality_id=100)
+        target = factories.StudentFactory(birthdate=None, nationality_id=models.NOT_KNOWN_NATIONALITY)
+
+        services.merge_students(source=source, target=target)
+
+        target.refresh_from_db()
+        self.assertEqual(source.birthdate, target.birthdate)  # standard field
+        self.assertEqual(source.nationality_id, target.nationality_id)  # notnull foreign key
+
+
+class TestHUSIDSort(SimpleTestCase):
+    def test_correct_order(self):
+        no_husid = factories.StudentFactory.build(husid=None)
+        _1999 = factories.StudentFactory.build(husid=9900000000000)
+        _2005 = factories.StudentFactory.build(husid=500000000000)
+        _2020 = factories.StudentFactory.build(husid=2000000000000)
+
+        unordered = [_2005, no_husid, _2020, _1999]
+        expected = [_1999, _2005, _2020, no_husid]
+        self.assertEqual(sorted(unordered, key=services._order_by_husid), expected)
