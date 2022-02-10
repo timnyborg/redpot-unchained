416666aefd10edc45004bdb8bf0108d67a57b530
Tim Nyborg <tim.nyborg@conted.ox.ac.uk>
2022-02-10 10:41:10 +0000

    sftp for public website media

--- redpot/storage_backends.py
+++ redpot/storage_backends.py
@@ -1,10 +1,16 @@
+from storages.backends.sftpstorage import SFTPStorage
+
 from django.conf import settings
-from django.core.files.storage import FileSystemStorage
 
 
-class WebsiteStorage(FileSystemStorage):
-    """Separate storage class for managing website media via mounted folder (e.g. module & tutor images)"""
+class WebsiteStorage(SFTPStorage):
+    """Separate storage class for managing website media over SFTP (e.g. module & tutor images)
 
-    location = settings.WEBSITE_MEDIA_ROOT
+    If an SFTP target isn't configured via settings, uploads will fail.
+    If this is a problem in dev, we may want a setting to choose between SFTPStorage and local filesystem storage
+    """
+
+    host = settings.WEBSITE_MEDIA_SFTP_HOST
+    params = settings.WEBSITE_MEDIA_SFTP_PARAMS
+    root_path = settings.WEBSITE_MEDIA_SFTP_ROOT
     base_url = settings.WEBSITE_MEDIA_URL
-    file_permissions_mode = None  # prevent failed chmod attempts while mounting via smb
