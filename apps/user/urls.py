d9aec80906ccff96bf92670e434b85a24b6e80b1
Tim Nyborg <tim.nyborg@conted.ox.ac.uk>
2022-02-17 17:12:33 +0000

    user creation and editing by admins

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'user'
 urlpatterns = [
     path('profile', views.EditProfile.as_view(), name='profile'),
+    path('edit/<int:pk>', views.EditProfile.as_view(), name='admin-edit'),
+    path('new', views.New.as_view(), name='new'),
 ]
