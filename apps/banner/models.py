687e8b41acddd7b654c6d01cf32431e0a8145c4a
Tim Nyborg <tim.nyborg@conted.ox.ac.uk>
2022-03-08 16:16:30 +0000

    banner display

--- apps/banner/models.py
+++ apps/banner/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 
 from django.db import models
 
@@ -7,9 +8,16 @@ class BannerQuerySet(models.QuerySet):
     def past(self) -> models.QuerySet:
         return self.filter(publish__lte=datetime.today())
 
+    def current(self) -> Optional['Banner']:
+        """Get the newest banner message for display"""
+        return self.filter(publish__lte=datetime.today()).exclude(unpublish__lte=datetime.today()).first()
+
 
 class Banner(models.Model):
-    """A feature, update, or alert message which appears atop the home page"""
+    """A feature, update, or alert message which appears atop the home page.
+    Todo: consider making banners appear atop every page, until dismissed (with a last-dismissed column on User?,
+      or just an "Updates!" link on the topnav which is marked unread until accessed
+    """
 
     class Types(models.IntegerChoices):
         NEW = 1, 'New feature'
