c99349ccd1e5b64b983e624235bd3c171685af5a
Tim Nyborg <tim.nyborg@conted.ox.ac.uk>
2022-04-26 10:40:15 +0100

    discount:create url to :new (consistency)

--- apps/discount/urls.py
+++ apps/discount/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'discount'
 
 urlpatterns = [
-    path('create', views.Create.as_view(), name='create'),
+    path('new', views.Create.as_view(), name='new'),
     path('edit/<int:pk>', views.Edit.as_view(), name='edit'),
     path('delete/<int:pk>', views.Delete.as_view(), name='delete'),
     path('search', views.Search.as_view(), name='search'),
